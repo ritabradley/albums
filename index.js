3cc841e5fd0d96c27ef057b35f24e7b47a569aa1
Rita Bradley <contact@ritabradley.com>
2019-07-21 14:47:46 -0700

    Render album art and details

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
-import AlbumList from './src/components/AlbumList'
+import AlbumList from './src/components/AlbumList';
 
 const App = () => {
-    return (
-        <View>
-            <Header headerText={'Albums'} />
-            <AlbumList />
-        </View>
-    )
-    };
+  return (
+    <View style={{ flex: 1 }}>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
+  );
+};
 
 AppRegistry.registerComponent('albums', () => App);
--- src/components/AlbumDetails.js
+++ src/components/AlbumDetails.js
@@ -1,14 +1,61 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetails = props => {
-  const { title, artist, url, image, thumbnail_image } = props.album;
+const AlbumDetails = ({ album }) => {
+  const { title, artist, url, image, thumbnail_image } = album;
+
+  const {
+    headerContentStyles,
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle,
+  } = styles;
   return (
-    <View>
-      <Text>{title}</Text>
-      <Text>{artist}</Text>
-    </View>
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerContentStyles}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+    </Card>
   );
+};
+
+const styles = {
+  headerContentStyles: {
+    justifyContent: 'space-around',
+    flexDirection: 'column',
+  },
+  headerTextStyle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
+  },
 };
 
 export default AlbumDetails;
